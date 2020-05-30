f3b2f8ac185f0c789626fb4f3d27d71bbdfd4947
jszh130 <jszh130@163.com>
2020-05-30 22:08:10 +0800

    homework of week2

---
+++ Week_02/[144]二叉树的前序遍历.py
@@ -0,0 +1,38 @@
+# 给定一个二叉树，返回它的 前序 遍历。 
+# 
+#  示例: 
+# 
+#  输入: [1,null,2,3]  
+#    1
+#     \
+#      2
+#     /
+#    3 
+# 
+# 输出: [1,2,3]
+#  
+# 
+#  进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+#  Related Topics 栈 树
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        def dfs(root):
+            if not root:
+                return
+            res.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        return res
+# leetcode submit region end(Prohibit modification and deletion)
---
+++ Week_02/[200]岛屿数量.py
@@ -0,0 +1,53 @@
+# 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。 
+# 
+#  岛屿总是被水包围，并且每座岛屿只能由水平方向或竖直方向上相邻的陆地连接形成。 
+# 
+#  此外，你可以假设该网格的四条边均被水包围。 
+# 
+#  
+# 
+#  示例 1: 
+# 
+#  输入:
+# 11110
+# 11010
+# 11000
+# 00000
+# 输出: 1
+#  
+# 
+#  示例 2: 
+# 
+#  输入:
+# 11000
+# 11000
+# 00100
+# 00011
+# 输出: 3
+# 解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+#  
+#  Related Topics 深度优先搜索 广度优先搜索 并查集
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, i, j):
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '0'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
+# leetcode submit region end(Prohibit modification and deletion)
